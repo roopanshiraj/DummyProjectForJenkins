45bb32dfc294a86f04cbb3d027ab08547ca0bbb9
hp <hp@192.168.1.9>
2020-06-14 02:47:08 +0530

    Pushing the code for the first time

---
+++ ForJenkins/src/test/java/PracticeMavenProject/ForJenkins/ClassA.java
@@ -0,0 +1,13 @@
+package PracticeMavenProject.ForJenkins;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+	
+	@Test
+	public void tc1()
+
+	{
+		System.out.println(" I am a testcase1");
+	}
+}
---
+++ ForJenkins/src/test/java/PracticeMavenProject/ForJenkins/ClassB.java
@@ -0,0 +1,14 @@
+package PracticeMavenProject.ForJenkins;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	
+	@Test
+	public void tc2()
+
+	{
+		System.out.println(" I am a testcase2");
+	}
+
+}
